b9cdecd639a4eefe7d82971be309597d0c941476
jakobthomassen <jakob.thomassen1@gmail.com>
2026-01-12 12:06:45 +0100

    __init__ in api/routes to solve routing discoverability

---
+++ backend/app/api/routes/__init__.py
@@ -0,0 +1,5 @@
+from .chat import router as chat_router
+from .debug import router as debug_router
+from .prompt_trace import router as prompt_trace_router
+
+__all__ = ["chat_router", "debug_router", "prompt_trace_router"]
