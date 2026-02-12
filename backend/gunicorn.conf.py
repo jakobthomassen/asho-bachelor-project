912699d4f3720445cca5036ab2ed57cc02fab00b
jakobthomassen <jakob.thomassen1@gmail.com>
2026-02-12 16:43:36 +0100

    logging update

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,3 +1,8 @@
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
+access_log_format = (
+    '%(h)s %(l)s %(u)s "%(r)s" %(s)s %(b)s '
+    '"%(f)s" "%(a)s" '
+    'ua="%({User-Agent}i)s" x_monitor_source="%({X-Monitor-Source}i)s"'
+)
